9daf64682e650022a241b38241eb97dec2501f31
CrisTafRos <cristian.tafolla.rosales@gmail.com>
2019-03-19 10:25:54 -0600

    ADD: se agrega index

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<title> Examen 1 </title>
+<head> </head>
+<body background="EEB0E6">
+<p> Esta sitio es mi Examen </p>
+
+<p> Me gusta la Sandia y cantar en ingles </p>
+<p> Mis vacaciones favoritas han sido a Orlando </p>
+<p> Mi deporte favorito es la Natacion y Amo a Daniel Radcliffe </p>
+
+<p> Funcion que convierte ACRES A METROS CUADRADOS </p>
+
+
+<a href="creditos.php">CREDITOS </a>
+
+
+
+</body>
+
+
+</html>
+
