24189dfae33fca952a1c1142a18e47d34dfff31a
CrisTafRos <cristian.tafolla.rosales@gmail.com>
2019-03-19 10:27:11 -0600

    ADD: se agrega creditos

---
+++ creditos.php
@@ -0,0 +1,15 @@
+<html>
+<title> CREDITOS <title>
+<head></head>
+<body>
+<div align="center"> CREDITOS 
+ELABORADO POR : </br>
+
+Donaji Hernandez Galicia de la materia WEB 2.0 </br>
+</div>
+
+<p> <a href="index.php"> Regresar a INICIO </a>
+
+</body>
+
+</html>
