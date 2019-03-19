267479a395e575eaba7a6896881a11d6e2a47c12
CrisTafRos <cristian.tafolla.rosales@gmail.com>
2019-03-19 10:45:21 -0600

    ADD: se modifica index

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <title> Examen 1 </title>
 <head> </head>
 <body background="EEB0E6">
-<p> Esta sitio es mi Examen </p>
-
+<p> Este sitio es mi Examen </p>
+</br> </br>
 <p> Me gusta la Sandia y cantar en ingles </p>
 <p> Mis vacaciones favoritas han sido a Orlando </p>
 <p> Mi deporte favorito es la Natacion y Amo a Daniel Radcliffe </p>
+</br> </br>
 
 <p> Funcion que convierte ACRES A METROS CUADRADOS </p>
 
